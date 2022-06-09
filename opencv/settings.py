48ef6dc731f2593665cdae1d87f59eb1c9f294f0
Sukham <schatha_be20@thapar.edu>
2022-06-09 21:47:25 +0530

    Commit

--- opencv/settings.py
+++ opencv/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,5 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
