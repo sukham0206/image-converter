d9cc366de344b5f7674e580dcc9876cc52a6514c
Sukham <schatha_be20@thapar.edu>
2022-06-09 19:16:00 +0530

    enter

---
+++ home/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from home.models import Form
+from home.modules import converter
+from django.core.mail import EmailMessage
+
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('Name')
+        size = request.POST.get('size')
+        email = request.POST.get('email')
+        file = request.FILES.get('file')
+        index = Form(name= name, size= size, email = email, file = file)
+        index.save()
+        converter.convert(index.file.path, size)
+        
+        
+        #email.send_email(str(request.POST.get('email')), index.file.path)
+        
+        send_email = EmailMessage(
+        'Hello',
+        'Thank you for using this service',
+        to=[email]
+        )
+        send_email.attach_file(index.file.path)
+        send_email.send()
+        
+    return render(request, 'index.html')
